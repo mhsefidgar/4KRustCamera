84a10198a6d92828daba19d05106d4cf7c567f8e
Mohammad Sefidgar <mhsefidgar@gmail.com>
2026-09-23 16:14:22 -0400

    Align shared frame transport for cross-process atomics

--- virtual-camera/FrameRing.h
+++ virtual-camera/FrameRing.h
@@ -9,15 +9,14 @@ constexpr uint32_t VC_BYTES = VC_STRIDE * VC_HEIGHT;
 constexpr wchar_t VC_MAPPING_NAME[] = L"Local\\4KRustCameraFrameRing";
 constexpr wchar_t VC_MUTEX_NAME[] = L"Local\\4KRustCameraFrameRingMutex";
 
-#pragma pack(push, 1)
 struct FrameHeader {
-    uint64_t sequence;       // even = stable, odd = writer is updating
-    uint64_t timestamp100ns; // QPC-correlated Media Foundation time
+    uint64_t sequence;
+    uint64_t timestamp100ns;
     uint32_t width;
     uint32_t height;
     uint32_t stride;
+    uint32_t reserved;
 };
-#pragma pack(pop)
 
 struct FrameRing {
     FrameHeader header;
